ce888c038b3d9d60149b16d2ccd2663ccfc7a74e
Saad Rana <saaderana@gmail.com>
2017-09-05 15:37:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+    url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyDAKDaBy_JDwcScSHqDQimOOLjdPImLanc',
+        q: searchTerm
+    };
+
+    $.getJSON(url, params, function (searchTerm) {
+        showResults(searchTerm);
+    });
+}
+
+function showResults(results) {
+    var html = "";
+    var entries = results.items;
+
+    $.each(entries, function (index, value) {
+        var title = value.snippet.title;
+        var thumbnail = value.snippet.thumbnails.default.url;
+        html += '<p>' + title + '</p>';
+        html += '<img src="' + thumbnail + '">';
+    });
+
+    $('#search-results').html(html);
+}
